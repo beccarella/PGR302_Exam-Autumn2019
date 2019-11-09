b3eea832e23c8409b04dc50d44cdec15508ed13d
beccarella <urquhart.becca@gmail.com>
2019-11-09 23:57:12 +0100

    Added react-bootstrap. Moved addNewPost btn to navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import { NavLink} from "react-router-dom";
+import AddPostModal from '../Components/AddPostModal'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavItem, Button } from 'react-bootstrap';
 
-const Header = () => {
-    return (
-        <div>
-            <nav className="navbar fixed-top navbar-light bg-light navbar-expand-md">
-                <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarNav">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
 
-                <div className="navbar-collapse collapse" id="navbarNav">
-                    <ul className="navbar-nav ml-auto text-center">
-                        <li className="nav-item"><NavLink exact to="/">Home</NavLink></li>
-                        <li className="nav-item"><NavLink exact to="/about">About</NavLink></li>
-                        <li className="nav-item"><NavLink exact to="/contact">Contact</NavLink></li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    )
+export default class Header extends React.Component {
+
+    AddPostModalRef = ({handleShow}) => {
+        this.showModal = handleShow;
+    }
+
+    onAddNewPostClick = () => {
+        this.showModal();
+    }
+    
+    render() {
+        return (
+            <Navbar collapseOnSelect expand="md" bg="light">
+                <Navbar.Brand href="#home">Good News</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">  
+                    <Nav className="mr.auto">
+                        <NavItem eventkey={1} href="/"><Nav.Link as={NavLink} to="/" >Home</Nav.Link></NavItem>
+                        <NavItem eventkey={2} href="/about"><Nav.Link as={NavLink} to="/about">About</Nav.Link></NavItem>
+                        <NavItem eventkey={3} href="/contact"><Nav.Link as={NavLink} to="/contact">Contact</Nav.Link></NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+                <Button variant="primary" onClick={this.onAddNewPostClick}>Add new article</Button>
+                <AddPostModal ref={this.AddPostModalRef}></AddPostModal>
+            </Navbar>
+        )
+    }
 }
-
-export default Header;
