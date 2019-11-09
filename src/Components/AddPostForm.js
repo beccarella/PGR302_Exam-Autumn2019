eddbbce614273aef5a7d0ba5eed1c4853c6f6127
beccarella <urquhart.becca@gmail.com>
2019-11-09 16:38:56 +0100

    Post is now functioning correctly :rocket:

--- src/Components/AddPostForm.js
+++ src/Components/AddPostForm.js
@@ -6,42 +6,62 @@ const webAPIUrl = "https://localhost:5001/newsarticles";
 
 export default class AddPostForm extends React.Component {
 
-    createPost() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: "",
+            ingress: "",
+            author: "",
+            date:"",
+            runningText: "",
+            category: ""
+        }
+    }
+
+    onChange = (event) => {
+        let newText = event.target.value;
+        this.setState({[event.target.name]: newText});
+    }
+
+    createPost = (event) => {
+        event.preventDefault();
+        const { title, ingress, author, date, runningText, category } = this.state;
         axios.post(
-            webAPIUrl
+            webAPIUrl, { title, ingress, author, date, runningText, category }
         )
     }
 
     render() {
+        const { title, ingress, author, date, runningText, category } = this.state;
         return(
             <Form onSubmit={this.createPost}>
                 <Form.Row>
                     <Form.Group as ={Col} controlId="formGridTitle">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control type="title" placeholder="Enter title" />
+                        <Form.Control type="text" name="title" value={title} placeholder="Enter title" onChange={this.onChange}/>
                     </Form.Group>
                     <Form.Group as ={Col} controlId="formGridIngress">
                         <Form.Label>Ingress</Form.Label>
-                        <Form.Control type="ingress" placeholder="Enter ingress" />
+                        <Form.Control type="text" name="ingress" value={ingress} placeholder="Enter ingress" onChange={this.onChange}/>
                     </Form.Group>
                 </Form.Row>
                 <Form.Row>
                     <Form.Group as ={Col} controlId="formGridAuthor">
                         <Form.Label>Author</Form.Label>
-                        <Form.Control type="Author" placeholder="Enter author name" />
+                        <Form.Control type="text" name="author" value={author} placeholder="Enter author name" onChange={this.onChange}/>
                     </Form.Group>
                     <Form.Group as ={Col} controlId="formGridDate">
                         <Form.Label>Date</Form.Label>
-                        <Form.Control type="date" placeholder="Enter date" />
+                        <Form.Control type="date" name="date" value={date} placeholder="Enter date" onChange={this.onChange}/>
                     </Form.Group>
                 </Form.Row>
                 <Form.Group controlId="formGridRunningText">
                     <Form.Label>Main text</Form.Label>
-                    <Form.Control placeholder="Enter article text" />
+                    <Form.Control type="text" name="runningText" value={runningText} placeholder="Enter article text" onChange={this.onChange}/>
                 </Form.Group>
                 <Form.Group controlId="formGridCategory">
                     <Form.Label>Category</Form.Label>
-                    <Form.Control placeholder="Enter category" />
+                    <Form.Control type="text" name="category" value={category} placeholder="Enter category" onChange={this.onChange}/>
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Publish                
