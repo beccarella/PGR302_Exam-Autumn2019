f21df4e520e8094d67f81ebe986d4f4e871df6f5
beccarella <urquhart.becca@gmail.com>
2019-11-16 21:34:48 +0100

    Now able to upload and post pictures

--- src/Components/AddPostModal.js
+++ src/Components/AddPostModal.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
+import {Modal, Form, Col, Button} from 'react-bootstrap';
 
-import AddPostForm from './AddPostForm';
+const webAPIUrl = "https://localhost:5001/newsarticles";
 
 export default class AddPostModal extends React.Component {
     constructor(props) {
@@ -9,18 +10,76 @@ export default class AddPostModal extends React.Component {
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
         this.state = {
-            show: false
+            show: false,
+            img: null,
+            title: "",
+            ingress: "",
+            author: "",
+            date:"",
+            runningText: "",
+            category: ""
         }
     }
 
-    handleShow() {
+    uploadImage (event) {
+        event.preventDefault();
+        let file = document.getElementById("upload-file");
+        let data = new FormData();
+
+        data.append("file", file.files[0]);
+        console.log(data);
+
+        let imgUrl = data.get('file');
+        this.state.img = imgUrl.name;
+
+        axios({
+            method: 'post',
+            url: 'https://localhost:5001/newsarticles/savePicture',
+            data: data,
+            config: {headers: {'Content-Type': 'multipart/form-data'}}
+        })
+        return imgUrl.name;
+    }
+
+
+    handleShow()  {
         this.setState({ show: true })
     }
+
     handleClose(){
         this.setState({ show: false })
     }
 
+    onChange = (event) => {
+        let newText = event.target.value;
+        this.setState({[event.target.name]: newText});
+    }
+
+    createPost = (event) => {
+        let selectedImg = this.uploadImage(event);
+        event.preventDefault();
+        let article = {
+            img: selectedImg,
+            title: this.state.title,
+            ingress: this.state.ingress,
+            author: this.state.author,
+            date: this.state.date,
+            runningText: this.state.runningText,
+            category: this.state.category
+        }
+        axios.post(webAPIUrl, article);
+    }
+
+/*     createPost = (event) => {
+        event.preventDefault();
+        const { title, ingress, author, date, runningText, category } = this.state;
+        axios.post(
+            webAPIUrl, { title, ingress, author, date, runningText, category }
+        )
+    } */
+
     render() {
+        const { title, ingress, author, date, runningText, category } = this.state;
         return(
             <>
                 <Modal show={this.state.show} onHide={this.handleClose}>
@@ -28,7 +87,41 @@ export default class AddPostModal extends React.Component {
                         <Modal.Title>Create new post</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <AddPostForm/>
+                        <Form onSubmit={this.createPost}>
+                            <Form.Group>
+                                <Form.Label>Image</Form.Label>
+                                <Form.Control type="file" name="img" id ="upload-file"/>
+                            </Form.Group>
+                            <Form.Row>
+                                <Form.Group as ={Col} controlId="formGridTitle">
+                                    <Form.Label>Title</Form.Label>
+                                    <Form.Control type="text" name="title" value={title} placeholder="Enter title" onChange={this.onChange}/>
+                                </Form.Group>
+                                <Form.Group as ={Col} controlId="formGridIngress">
+                                    <Form.Label>Ingress</Form.Label>
+                                    <Form.Control type="text" name="ingress" value={ingress} placeholder="Enter ingress" onChange={this.onChange}/>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as ={Col} controlId="formGridAuthor">
+                                    <Form.Label>Author</Form.Label>
+                                    <Form.Control type="text" name="author" value={author} placeholder="Enter author name" onChange={this.onChange}/>
+                                </Form.Group>
+                                <Form.Group as ={Col} controlId="formGridDate">
+                                    <Form.Label>Date</Form.Label>
+                                    <Form.Control type="date" name="date" value={date} placeholder="Enter date" onChange={this.onChange}/>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Group controlId="formGridRunningText">
+                                <Form.Label>Main text</Form.Label>
+                                <Form.Control type="text" name="runningText" value={runningText} placeholder="Enter article text" onChange={this.onChange}/>
+                            </Form.Group>
+                            <Form.Group controlId="formGridCategory">
+                                <Form.Label>Category</Form.Label>
+                                <Form.Control type="text" name="category" value={category} placeholder="Enter category" onChange={this.onChange}/>
+                            </Form.Group>
+                            <Button variant= "primary" type="submit" onClick={this.handleClose}>Save and close</Button>
+                        </Form>
                     </Modal.Body>
                 </Modal>
             </>
--- src/Components/ArticleCard.js
+++ src/Components/ArticleCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-import { Card, Button, ButtonToolbar, Container } from 'react-bootstrap';
+import { Card, Button, ButtonToolbar, Container, Row, Form} from 'react-bootstrap';
+
 
 const webAPIUrl = "https://localhost:5001/newsarticles";
 
@@ -10,33 +11,35 @@ const ArticleCard = ({news}) => {
         axios.delete(`${webAPIUrl}/${id}`)
         .then(res => {
             console.log(res)
+            window.location.reload();
         })
-        window.location.reload();
     }
 
     if(news){
         const nNews = news.map((n, index) => {
             return(
-                <Card key={index} style={{width: '18rem' }}>
-                    <Card.Img variant="top" src={`/assets/placeholder.png`} alt="placeholder" />
+                <Card key={index} style={{width: '18rem' }} id="card">
+                    <Card.Img variant="top" src={`https://localhost:5001/images/${n.img}`} fluid="true" />
                     <Card.Body>
                         <Card.Title>{n.title}</Card.Title>
                         <Card.Subtitle>{n.ingress} </Card.Subtitle>
                         <Card.Text>author: {n.author}</Card.Text>
                         <Card.Text>{n.date}</Card.Text>
-                        <Card.Text>{n.runningText}</Card.Text>
+                        <Card.Text>{n.runningText}</Card.Text>                            
                         <Card.Text>category: {n.category}</Card.Text>
+                        <ButtonToolbar>
+                            <Button variant="outline-info">Edit</Button>
+                            <Button onClick={deletePost.bind(this, n.id)} variant="outline-danger">Delete</Button>
+                        </ButtonToolbar>
                     </Card.Body>
-                    <ButtonToolbar>
-                        <Button variant="outline-info">Edit</Button>
-                        <Button onClick={deletePost.bind(this, n.id)} variant="outline-danger">Delete</Button>
-                    </ButtonToolbar>
                 </Card>
             )
         })
         return(
-            <Container className="row justify-content-center">
-                {nNews}
+            <Container>
+                <Row>   
+                    {nNews}
+                </Row>
             </Container>
         )
     }
